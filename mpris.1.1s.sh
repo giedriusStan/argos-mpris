b2ad634cb95058f39af64663aba98fb130df8a7f
Seifer23 <48915360+Seifer23@users.noreply.github.com>
2021-11-12 13:16:06 +0100

    Display podcasts correctly and track info
    When a podcast is playing, instead of " - [Episode Name]" it will display "[Podcast Name] - [Episode Name]".
    
    Also, now the name, author and album of a track are visible on the menu

--- mpris.1.1s.sh
+++ mpris.1.1s.sh
@@ -9,17 +9,33 @@ PREVIOUS="$COMMAND_BASE org.mpris.MediaPlayer2.Player.Previous"
 MPRIS_META=$($COMMAND_BASE org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata)
 ARTIST=$(echo "$MPRIS_META" | sed -n '/artist/{n;n;p}' | cut -d '"' -f 2)
 SONG_TITLE=$(echo "$MPRIS_META" | sed -n '/title/{n;p}' | cut -d '"' -f 2)
+ALBUM=$(echo "$MPRIS_META" | sed -n '/album/{n;p}' | cut -d '"' -f 2 | head -1)
 
 PLAYBACK_STATUS=$($COMMAND_BASE org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:PlaybackStatus)
+
 if [[ $PLAYBACK_STATUS == *"Playing"* ]]; then
-  TITLE="$ARTIST - $SONG_TITLE | iconName=media-playback-start"
   PLAY_PAUSE_TOGGLE="Pause | iconName=media-playback-pause bash='$PLAY_PAUSE' terminal=false refresh=true"
 else
-  TITLE="$ARTIST - $SONG_TITLE | iconName=media-playback-pause"
   PLAY_PAUSE_TOGGLE="Play | iconName=media-playback-start bash='$PLAY_PAUSE' terminal=false refresh=true"
 fi
 
+if [[ $ARTIST == "" ]]; then
+	TITLE="$ALBUM - $SONG_TITLE | iconName=media-tape"
+else
+	TITLE="$ARTIST - $SONG_TITLE | iconName=media-playback-pause"
+fi
+
 echo "$TITLE"
+echo "---"
+echo "$SONG_TITLE | iconName=folder-music-symbolic"
+
+if [[ $ARTIST == "" ]]; then
+	echo "$ALBUM | iconName=media-tape"
+else
+	echo "$ARTIST | iconName=contact-new"
+	echo "$ALBUM | iconName=media-optical"
+fi
+
 echo "---"
 echo "Previous | iconName=media-skip-backward bash='$PREVIOUS' terminal=false refresh=true"
 echo "Next | iconName=media-skip-forward bash='$NEXT' terminal=false refresh=true"
